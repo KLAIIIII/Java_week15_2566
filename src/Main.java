03d0e9209fa006ac559a1689d121eec81a5b5140
Jimmyseesang <jimmyseesang@gmail.com>
2023-01-26 10:50:52 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 interface Fruit{
-    String color = "Red color fruit, Interface";
+    String color = "Red color fruit , Interface";
     void favorite();
     default void province(){
         System.out.println("Chanthaburi");
+    }
+    interface Message{
+        void msg();
     }
 }
 
@@ -10,7 +13,11 @@ interface Brand{
     void name();
 }
 
-class Apple implements Fruit,Brand{
+class Apple implements Fruit,Brand,Fruit.Message{
+
+    public void msg(){
+        System.out.println(Fruit.color + "Nested Interface");
+    }
     @Override
     public void name() {
         System.out.println("Dole brand");
@@ -29,9 +36,11 @@ public class Main {
         Apple objApp = new Apple();
         Apple objApp2 = new Apple();
         Apple objApp3 = new Apple();
+        Fruit.Message objApp4 = new Apple();
 
+        objApp4.msg();
         objApp3.name();
-        objApp.favorite();
         objApp2.province();
+        objApp.favorite();
     }
 }
